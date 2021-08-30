d5e1cbf474f9584ee9978c69edb6db264ea4231b
Giriraj-Roy <88903134+Giriraj-Roy@users.noreply.github.com>
2021-08-30 23:41:05 +0530

    Update settings.py

--- FakeNewsDetector/settings.py
+++ FakeNewsDetector/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!87n^(@wkpe#3n2)y4qt4^*x-ep$90l4!3j!t+*gy9__pvr!hu'
+#SECRET_KEY = 'django-insecure-!87n^(@wkpe#3n2)y4qt4^*x-ep$90l4!3j!t+*gy9__pvr!hu'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
